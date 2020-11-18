1e2a1fc4c3bd2c8536281895c440ba219040b7f6
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2020-11-18 17:02:01 -0300

    commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import { Link } from 'react-router-dom';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 import { Container } from './styles';
 
@@ -10,17 +10,20 @@ interface HeaderProps {
   size?: 'small' | 'large';
 }
 
-const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
-  <Container size={size}>
-    <header>
-      <img src={Logo} alt="GoFinances" />
-      <nav>
-        {
-          // Todo
-        }
-      </nav>
-    </header>
-  </Container>
-);
+const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => {
+  const { path } = useRouteMatch();
+
+  return (
+    <Container size={size}>
+      <header>
+        <img src={Logo} alt="GoFinances" />
+        <nav>
+          {path === '/' && <Link to="/import">Importar</Link>}
+          {path === '/import' && <Link to="/">{`< Voltar`}</Link>}
+        </nav>
+      </header>
+    </Container>
+  );
+};
 
 export default Header;
